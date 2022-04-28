41e9235dc43a63af614350212e452d5bea7bdc3b
Maxim Pomogaev <coldfrontlich@gmail.com>
2022-04-29 00:34:45 +0300

    Make tests

--- SimpleGame/src/SimpleDot.java
+++ SimpleGame/src/SimpleDot.java
@@ -1,6 +1,13 @@
 public class SimpleDot {
     int[] locationCells;
-    int numOfHits;
+    private int numOfHits = 0;
 
+    public String checkYourself(String guess) {
+        return null;
+    }
+
+    public void setLocationCells(int[] location) {
+
+    }
 
 }
--- SimpleGame/src/SimpleGame.java
+++ SimpleGame/src/SimpleGame.java
@@ -1,5 +1,20 @@
 public class SimpleGame {
     public static void main(String[] args) {
+        SimpleDot dot = new SimpleDot();
 
+        int[] locations = {2, 3, 4};
+
+        dot.setLocationCells(locations);
+
+        String userGuess = "2";
+        String result = dot.checkYourself(userGuess);
+
+        String testResult = "Неудача";
+
+        if (result.equals("Попал")) {
+            testResult = "Попал";
+        }
+
+        System.out.println(testResult);
     }
 }
